f85f2b8ab0bcfa5dcae96493cc325edcdbe4f13e
Matthias L. Jugel <leo@thinkberg.com>
2016-05-20 13:38:44 +0200

    fix typo, include only selected board in docs

--- board/frdm_k82f/board.c
+++ board/frdm_k82f/board.c
@@ -23,7 +23,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
-  * limitations under the License. \n * ```\n
+ * limitations under the License.
+ * ```
  */
 
 #include "board.h"
