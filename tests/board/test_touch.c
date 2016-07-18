af65b7636e46c20c3f3d5829108441de7023a898
SteffCGN <graunke@gmail.com>
2016-07-18 15:19:40 +0200

    touch optimizations

--- tests/board/test_touch.c
+++ tests/board/test_touch.c
@@ -29,6 +29,7 @@ void TSI0_IRQHandler(void) {
   if (TSI_GetMeasuredChannelNumber(TSI0) == BOARD_TSI_ELECTRODE_1) {
     if (TSI_GetCounter(TSI0) > (buffer.calibratedData[BOARD_TSI_ELECTRODE_1] + 100U)) {
       touched = true;
+      PRINTF("TOUCH");
     }
     PRINTF("\e[KChannel %d (%d)\r", BOARD_TSI_ELECTRODE_1, TSI_GetCounter(TSI0));
   }
@@ -36,6 +37,7 @@ void TSI0_IRQHandler(void) {
   if (TSI_GetMeasuredChannelNumber(TSI0) == BOARD_TSI_ELECTRODE_2) {
     if (TSI_GetCounter(TSI0) > (buffer.calibratedData[BOARD_TSI_ELECTRODE_2] + 100U)) {
       touched = true;
+      PRINTF("TOUCH");
     }
     PRINTF("\e[KChannel %d (%d)\r", BOARD_TSI_ELECTRODE_2, TSI_GetCounter(TSI0));
   }
@@ -53,6 +55,7 @@ void test_touch() {
   /* Affects PORTB_PCR19 register */
   PORT_SetPinMux(PORTB, 17U, kPORT_PinDisabledOrAnalog);
 
+
   volatile uint32_t i = 0;
   tsi_config_t tsiConfig_normal = {0};
   lptmr_config_t lptmrConfig;
@@ -67,7 +70,13 @@ void test_touch() {
   LPTMR_Init(LPTMR0, &lptmrConfig);
   /* Initialize the TSI */
   TSI_Init(TSI0, &tsiConfig_normal);
+  //TSI_SetNumberOfScans(TSI0,5);
 
+
+  //TSI_SetLowThreshold(TSI0, 3900);
+  TSI_SetElectrodeChargeCurrent(TSI0,kTSI_ExtOscChargeCurrent_1uA);
+  TSI_SetElectrodeSeriesResistor(TSI0,kTSI_SeriesResistance_32k);
+  TSI_SetNumberOfScans(TSI0,kTSI_ConsecutiveScansNumber_32time);
   /* Set timer period */
   LPTMR_SetTimerPeriod(LPTMR0, (uint16_t) USEC_TO_COUNT(500000U, LPTMR_SOURCE_CLOCK));
 
@@ -93,7 +102,7 @@ void test_touch() {
   TSI_EnableInterrupts(TSI0, kTSI_EndOfScanInterruptEnable);
   TSI_ClearStatusFlags(TSI0, kTSI_EndOfScanFlag);
 
-  TSI_SetMeasuredChannelNumber(TSI0, BOARD_TSI_ELECTRODE_1); /* Select TSI_CHANNEL_1 as detecting electrode. */
+  TSI_SetMeasuredChannelNumber(TSI0, BOARD_TSI_ELECTRODE_2); /* Select TSI_CHANNEL_1 as detecting electrode. */
   TSI_EnableModule(TSI0, true);
   LPTMR_StartTimer(LPTMR0); /* Start LPTMR triggering */
 
