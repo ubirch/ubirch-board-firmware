4fb16efd351f8b9b6273f33a653f4c6db8b9de82
Matthias L. Jugel <leo@thinkberg.com>
2016-08-31 23:10:49 +0200

    first go at audio output

--- tests/board/support.h
+++ tests/board/support.h
@@ -29,14 +29,14 @@
 
 enum {
     enable_input = false,         //!< use serial console input to start/continue tests
-    enable_test_audio = false,
+    enable_test_audio = true,
     enable_test_gpio = false,
     enable_test_pir = false,
     enable_test_quectel = false,
     enable_test_rgb_dma = false,
     enable_test_rgb_flexio = false,
     enable_test_touch = false,
-    enable_test_sdcard = true,
+    enable_test_sdcard = false,
     enable_test_i2s = false,
 };
 
