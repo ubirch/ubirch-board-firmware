f5ef7305b80d9b752aac34caebea10b1af4d353e
Niranjan Rao <niranjan.rao@ubirch.com>
2016-10-12 13:59:07 +0200

    qfread bug fix can read 10 bytes from a file - but only using readline fun not read_binary - yes it is wierd

--- tests/modem_http/main.c
+++ tests/modem_http/main.c
@@ -49,7 +49,7 @@ int main(void) {
         PRINTF("Failed to connect to the server");
         return false;
       } else {
-        modem_send("AT+QHTTPGET=%d", 40);
+        modem_send("AT+QHTTPGET=40");
         if (!modem_expect_OK(40 * 1000)) {
           char get_response[] = {0};
           modem_readline(get_response, 20, 3 * 5000);
@@ -59,36 +59,48 @@ int main(void) {
       }
     }
 
-    static uint32_t dl_len, content_len = 0;
-    static uint16_t error_code = 0;
+//    modem_send("AT+QHTTPREAD=60");
+//    modem_expect_OK(5000);
+//    uint8_t read_buff[] = {0};
+//    modem_http_read(read_buff, 10 * 6000);
+    int dl_len, content_len, error_code = 0;
     const char file_name[] = "RAM:text1.txt";
 
     modem_send("AT+QHTTPDL=\"%s\"", file_name);
-    modem_expect_scan("+QHTTPDL: %d,%d,%d", 10 * 5000, &dl_len, &content_len, &error_code);
+    if (modem_expect_OK(5 * 1000)) PRINTF("OK HTTPDL siktu guru\r\n");
+    if (modem_expect_scan("+QHTTPDL: %d,%d,%d", 10 * 5000, &dl_len, &content_len, &error_code))
+//    if (modem_expect("+QHTTPDL: 2048,2048,0", 5 * 2000))
+    {
+//      PRINTF("no file handle number lfasdfasdf\r\n");
+      PRINTF("dl_len = %d, content_len = %d, error_code = %d\r\n", dl_len, content_len, error_code);
+    }
 
-    PRINTF("dl_len = %d, content_len = %d, error_code = %d\r\n", dl_len, content_len, error_code);
-    if (!modem_expect_OK(10 * 5000)) PRINTF("HTTPDL OK not received\r\n");
+//    if (!modem_expect_OK(10 * 5000)) PRINTF("HTTPDL OK not received\r\n");
 
     PRINTF("Now opening file\r\n");
     int file_handle = 0;
-    uint8_t rw_mode = 2;
+    uint8_t rw_mode = 0;
     uint16_t len = 10;
     char read_buffer[] = {0};
 
-    modem_send("AT+QFOPEN=\"%s\",%u", file_name, rw_mode);
-    if (modem_expect_scan("+QFOPEN: %d", 5 * 1000, &file_handle))
+    modem_send("AT+QFOPEN=\"%s\",%d", file_name, rw_mode);
+    if (modem_expect_scan("+QFOPEN: %u", 5 * 1000, &file_handle))
     {
       PRINTF("File handle number %u\r\n", file_handle);
     }
 
     if (!modem_expect_OK(10 * 3000)) PRINTF("No file handle received\r\n");
 
-    modem_send("AT+QFREAD=%u,%d", file_handle, len);
+    modem_send("AT+QFREAD=%d,%d", file_handle, len);
+    size_t  data_len = modem_readline(read_buffer, 10, 5 * 5000);
 //    size_t data_len = modem_read_binary((uint8_t *) read_buffer, len, 5 * 5000);
-//    CIODEBUG("HTTP (%02d) -> '%s'\r\n", strlen(read_buffer), data_len);
-//    if (!modem_expect_OK(50000)) PRINTF("Nothing read\r\n");
+    CIODEBUG("HTTP (%02d) -> '%s'\r\n", strlen(read_buffer), data_len);
+    if (!modem_expect_OK(50000)) PRINTF("Nothing read\r\n");
 
-    http_file_close(file_handle);
+//    http_file_close(file_handle);
+    modem_send("AT+QFCLOSE=%d", file_handle);
+    if (!modem_expect_OK(2000)) PRINTF("sariyagi close aagilla\r\n")
+        ;
     counter--;
     delay(10 * 1000);
   }
