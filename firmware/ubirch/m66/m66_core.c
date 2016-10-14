8105ac3c9697e8bb6c8f2d53e8b6a40a5f0a369f
Niranjan Rao <niranjan.rao@ubirch.com>
2016-10-14 10:49:17 +0200

    revert interrut handlers

--- firmware/ubirch/m66/m66_core.c
+++ firmware/ubirch/m66/m66_core.c
@@ -46,13 +46,13 @@ void BOARD_CELL_UART_IRQ_HANDLER(void) {
     // it may be necessary to create a critical section here, but
     // right now it didn't hurt us to not disable interrupts
 
-     __disable_irq();
+//     __disable_irq();
     /* If ring buffer is not full, add data to ring buffer. */
     if (((gsmRxIndex + 1) % GSM_RINGBUFFER_SIZE) != gsmRxHead) {
       gsmUartRingBuffer[gsmRxIndex++] = data;
       gsmRxIndex %= GSM_RINGBUFFER_SIZE;
     }
-     __enable_irq();
+//     __enable_irq();
   }
 }
 
