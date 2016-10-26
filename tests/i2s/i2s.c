3110ed90d19463f00c35f2375af433087c3cea61
Niranjan Rao <niranjan.rao@ubirch.com>
2016-10-26 12:52:08 +0200

    irq_handlers for SAI_READ

--- tests/i2s/i2s.c
+++ tests/i2s/i2s.c
@@ -4,14 +4,15 @@
 #include <fsl_sai.h>
 #include <ubirch/dbgutil.h>
 #include <ubirch/timer.h>
-#include <ubirch/ws2812b.h>
-#include "tosdcard.h"
 
 bool isFinished = false;
-uint32_t temp[3 * 2048];
-uint8_t buffer_write[255];
-sai_handle_t rxHandle = {0};
 
+static void SAI_ReadNonBlocking(I2S_Type *base, uint32_t channel, uint32_t bitWidth);
+
+static uint32_t temp[3 * 2048];
+static size_t temp_size = sizeof(temp);
+
+sai_handle_t rxHandle = {0};
 static sai_config_t config;
 sai_transfer_format_t format;
 sai_transfer_t xfer;
@@ -22,19 +23,44 @@ static void callback(I2S_Type *base, sai_handle_t *handle, status_t status, void
   {
     isFinished = true;
   }
-  else
+}
+
+#define SAI_IRQ_HANDLER I2S0_Rx_IRQHandler
+#define SAI_IRQ I2S0_Rx_IRQn
+
+void SAI_IRQ_HANDLER(I2S_Type *base, sai_handle_t *handle)
+{
+  if (base->RCSR & I2S_RCSR_FWF_MASK)
   {
-    PRINTF("STATUS is %x\r\n", status);
+    SAI_ReadNonBlocking(I2S0, handle->channel, handle->bitWidth);
+    isFinished = true;
+  }
+}
+
+static void SAI_ReadNonBlocking(I2S_Type *base, uint32_t channel, uint32_t bitWidth)
+{
+  uint32_t i = 0;
+  uint8_t j = 0;
+  uint8_t bytesPerWord = bitWidth / 8U;
+  uint32_t data = 0;
+
+  for (i = 0; i < temp_size / bytesPerWord; i++)
+  {
+//    data = base->RDR[channel];
+    data = SAI_ReadData(I2S0, format.channel);
+    for (j = 0; j < bytesPerWord; j++)
+    {
+      temp[j] = (data >> (8U * j)) & 0xFF;
+//      temp++;
+    }
   }
 }
 
 void sai_init(sai_config_t config)
 {
   PRINTF("Enable I2S pins...\r\n");
-  CLOCK_EnableClock(kCLOCK_PortA);
 
-  const gpio_pin_config_t OUTTRUE = {kGPIO_DigitalOutput, true};
-  const gpio_pin_config_t IN = {kGPIO_DigitalInput, false};
+  CLOCK_EnableClock(kCLOCK_PortA);
 
   // DM_DO (Data) and // DM_WS (FS, Word/Frame Sync)
   port_pin_config_t pin_config = {0};
@@ -42,20 +68,14 @@ void sai_init(sai_config_t config)
   pin_config.pullSelect = kPORT_PullDown;
   pin_config.mux = kPORT_MuxAlt6;
 
-  PORT_SetPinConfig(PORTA, 15U, &pin_config);
-  PORT_SetPinConfig(PORTA, 16U, &pin_config);
+  PORT_SetPinConfig(PORTA, 15U, &pin_config);  //DO
+  PORT_SetPinConfig(PORTA, 16U, &pin_config);  //WS
 
-//  PORT_SetPinMux(PORTA, 16U, kPORT_MuxAlt6);    // WS
-  PORT_SetPinMux(PORTA, 15U, kPORT_MuxAlt6);    // DM_DO
-  PORT_SetPinMux(PORTA, 16U, kPORT_MuxAlt6);  // DM_WS
   PORT_SetPinMux(PORTA, 17U, kPORT_MuxAlt6);  // DM_CLK (MCLK/BCLK)
 
   PORT_SetPinMux(PORTA, 18U, kPORT_MuxAsGpio);  // DM_SEL
-  GPIO_PinInit(BOARD_CELL_PIN_GPIO, BOARD_CELL_STATUS_PIN, &IN);
-
 
   const gpio_pin_config_t OUT = {kGPIO_DigitalOutput, 0};
-//  GPIO_PinInit(GPIOA, 16U, &OUT);
   GPIO_PinInit(GPIOA, 18U, &OUT);
 
   PRINTF("Configuring SAI...\r\n");
@@ -66,7 +86,7 @@ void sai_init(sai_config_t config)
   SAI_RxInit(I2S0, &config);
 
   /* Configure the audio format */
-  format.bitWidth = kSAI_WordWidth24bits;
+  format.bitWidth = kSAI_WordWidth32bits;
   format.channel = 0U;
   format.sampleRate_Hz = kSAI_SampleRate32KHz;
   format.masterClockHz = 64U * format.sampleRate_Hz;
@@ -75,7 +95,6 @@ void sai_init(sai_config_t config)
   format.watermark = FSL_FEATURE_SAI_FIFO_COUNT / 2U;
 
   SAI_TransferRxCreateHandle(I2S0, &rxHandle, callback, NULL);
-//  SAI_RxSetFormat
   SAI_TransferRxSetFormat(I2S0, &rxHandle, &format, CLOCK_GetCoreSysClkFreq(), format.masterClockHz);
 }
 
@@ -98,45 +117,31 @@ int main (void)
   board_init();
   board_console_init(BOARD_DEBUG_BAUD);
 
-//  delay(1000);
-//  ws2812b_send(0x000011, 2);
-//  delay(1000);
-
   SysTick_Config(BOARD_SYSTICK_100MS / 10);
-
-//  init_sdhc_pins();
 
   sai_init(config);
 
   PRINTF("Listening...\r\n");
-  GPIO_WritePinOutput(GPIOA, 18U, true);
 
-//  delay(2000);
-//  GPIO_WritePinOutput(GPIOA, 16U, true);
-//  delay(2000);
 
   xfer.data = (uint8_t *) temp;
   xfer.dataSize = sizeof(temp);
-//  xfer.data = buffer_write;
-//  xfer.dataSize = sizeof(buffer_write);
-//  PRINTF("FINISHED.\r\nDATA size: %d", xfer.dataSize);
 
+  SAI_RxEnableInterrupts(I2S0, kSAI_FIFOErrorInterruptEnable | kSAI_FIFOWarningInterruptEnable);
+  EnableIRQ(SAI_IRQ);
 
+  GPIO_WritePinOutput(GPIOA, 18U, true);
 
+//  delay(3000);
 //  PRINTF("%x\r\n", SAI_TransferReceiveNonBlocking(I2S0, &rxHandle, &xfer));
-  SAI_ReadBlocking(I2S0, 0, kSAI_WordWidth24bits, xfer.data, xfer.dataSize);
-  dbg_xxd("AUDIO", xfer.data, xfer.dataSize);
-//  pcm__sdhc(buffer_write);
-//  delay(2000);
-
-//  GPIO_WritePinOutput(GPIOA, 16U, false);
-  GPIO_WritePinOutput(GPIOA, 18U, false);
 
   while (isFinished != true) {}
+  GPIO_WritePinOutput(GPIOA, 18U, false);
 
-  PRINTF("FINISHED.\r\n"); //DATA size: %d\r\n", xfer.dataSize);
-  PRINTF("DATA is %x\r\n", buffer_write[2]);
-  dbg_xxd("AUDIO", buffer_write, xfer.dataSize);
+
+  PRINTF("FINISHED.\r\n");
+//  PRINTF("DATA is %x\r\n", xfer.dataSize);
+  dbg_xxd("AUDIO", (uint8_t *)temp, temp_size);
 
   while(true)
   {
